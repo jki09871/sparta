d540621d5d04e184a71af8d3ccbabb03afa3b316
jki09871 <ghdwjdrl56@naver.com>
2024-07-25 16:19:02 +0900

    1-7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,17 +1,17 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
-        int[] resultSave = new int[10];
-        int index = 0;
+        List<Integer> resultSave = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-
+        int answer; // 계산된 값이 저장될 변수
         do {
-
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num = sc.nextInt();
 
@@ -22,7 +22,6 @@ public class App {
             // 문자열을 읽고 charAt(0) 문자열에 몇번째것을 저장할건지 선택 (0번은 첫번째 문자를 읽어옴)
             char operator = sc.next().charAt(0);
 
-            int answer = 0;
 
             if (operator == '+') {
                 answer = num + num2;
@@ -31,7 +30,7 @@ public class App {
             } else if (operator == '*') {
                 answer = num * num2;
             } else if (operator == '/') {
-                if (num2 != 0) {
+                if (num2 != 0) {    // num2에 0값이 입력됐는지 확인
                     answer = num / num2;
                 } else {
                     System.out.println("두번째 입력한 숫자는 0이 될 수 없습니다.");
@@ -42,26 +41,19 @@ public class App {
                 return;
             }
 
-            if (index == 10) {
 
-                for (int i = 0; i < resultSave.length - 1; i++) { // 0 ~ 8
-                    resultSave[i] = resultSave[i + 1];
-                    // resultSave[0] = resultSave[0 + 1] : resultSave[1]에 저장된 값을 resultSave[0]에 저장한다.
-                    // 배열 값은 0 부터 시작하기 때문에 resultSave[8 + 1] =9 이다 9는 마지막 10번째 자리이다.
-                    // 반복해서 i가 9되면 for문을 빠져 나온다.
-                }
-                // 하나씩 이동 후 10인 index값을 1 빼주면 9가 된다.
-                index--;
+            resultSave.add(answer); // 계산된 값을 List에 넣는다.
+            System.out.println(num + "" + operator + "" + num2 + "=" + answer);
+            System.out.println("resultSave 저장된 값 => " + resultSave.toString()); // List에 저장된 값 보기.
+
+            System.out.print("첫번째 값을 삭제하시겠습니까? (remove 입력 시 삭제) : ");
+            if (sc.next().equals("remove")) { // remove 입력시 List에 첫번째 값을 삭제
+                resultSave.remove(0);
+                System.out.println("resultSave 저장된 값 => " + resultSave.toString());
             }
-            // resultSave[9] 는 10번째 자리이며 그 자리에 계산된 값을 넣는다.
-            resultSave[index] = answer;
-            // 다시 index값을 10으로 만들고 계속을 계속하게 되면 다시 for문에 들어가서 반복이 된다.
-            index++;
-            System.out.println( num +""+ operator +""+ num2 +"="+answer);
-            System.out.println("Arrays.toString(resultSave) = " + Arrays.toString(resultSave));
-
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : ");
         } while (!sc.next().equals("exit"));
         System.out.println("계산이 종료되었습니다.");
     }
+
 }

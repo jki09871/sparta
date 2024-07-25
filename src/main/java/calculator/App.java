eee1ad5f279dbe7e7c01afab4db3c1ae95c73eaa
jki09871 <ghdwjdrl56@naver.com>
2024-07-25 15:36:33 +0900

    1-6

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -11,19 +11,16 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         do {
+
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num = sc.nextInt();
+
             System.out.print("두 번째 숫자를 입력하세요: ");
             int num2 = sc.nextInt();
 
-            System.out.println("num =" + num);
-            System.out.println("num2 =" + num2);
-
             System.out.print("사칙연산 기호를 입력하세요: ");
-            // 문자열을 읽고 charAt(0) 문자열에 몇번째것을 저장할건지 선택 (0번은 첫번째 문자열을 읽어옴)
+            // 문자열을 읽고 charAt(0) 문자열에 몇번째것을 저장할건지 선택 (0번은 첫번째 문자를 읽어옴)
             char operator = sc.next().charAt(0);
-
-            System.out.println("operator = " + operator);
 
             int answer = 0;
 
@@ -45,18 +42,26 @@ public class App {
                 return;
             }
 
-            System.out.println("결과: " + answer);
+            if (index == 10) {
+
+                for (int i = 0; i < resultSave.length - 1; i++) { // 0 ~ 8
+                    resultSave[i] = resultSave[i + 1];
+                    // resultSave[0] = resultSave[0 + 1] : resultSave[1]에 저장된 값을 resultSave[0]에 저장한다.
+                    // 배열 값은 0 부터 시작하기 때문에 resultSave[8 + 1] =9 이다 9는 마지막 10번째 자리이다.
+                    // 반복해서 i가 9되면 for문을 빠져 나온다.
+                }
+                // 하나씩 이동 후 10인 index값을 1 빼주면 9가 된다.
+                index--;
+            }
+            // resultSave[9] 는 10번째 자리이며 그 자리에 계산된 값을 넣는다.
             resultSave[index] = answer;
+            // 다시 index값을 10으로 만들고 계속을 계속하게 되면 다시 for문에 들어가서 반복이 된다.
             index++;
-//            가장 기본적이게 보는 방법 1첫째 방법
-//            for (int i = 0; i < resultSave.length; i++){
-//                System.out.println(i +"번째 결과 = " + resultSave[i]);
-//            }
-//            배열의 값을 더 편하게 볼 수있는 방법
-            System.out.println("Arrays.toString(resultSave) = "+Arrays.toString(resultSave));
+            System.out.println( num +""+ operator +""+ num2 +"="+answer);
+            System.out.println("Arrays.toString(resultSave) = " + Arrays.toString(resultSave));
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-        }while (!sc.next().equals("exit"));
+        } while (!sc.next().equals("exit"));
         System.out.println("계산이 종료되었습니다.");
     }
 }

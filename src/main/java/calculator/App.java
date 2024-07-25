ac3093f85a6965c581d78b1192cfc78aa6a67065
jki09871 <ghdwjdrl56@naver.com>
2024-07-25 10:18:19 +0900

    1-2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,13 +8,17 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num = sc.nextInt();
         System.out.print("두 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num2 = sc.nextInt();
 
         System.out.println("num =" + num);
         System.out.println("num2 =" + num2);
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        // 문자열을 읽고 charAt(0) 문자열에 몇번째것을 저장할건지 선택 (0번은 첫번째 문자열을 읽어옴)
+        char operator= sc.next().charAt(0);
+
+        System.out.println("operator = " + operator);
     }
 }

123711d82e0f926bb3cd7ca06c4d17cdcd449036
jki09871 <ghdwjdrl56@naver.com>
2024-07-25 12:05:34 +0900

    1-4

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,40 +7,44 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
+        do {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
 
-        System.out.println("num =" + num);
-        System.out.println("num2 =" + num2);
+            System.out.println("num =" + num);
+            System.out.println("num2 =" + num2);
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        // 문자열을 읽고 charAt(0) 문자열에 몇번째것을 저장할건지 선택 (0번은 첫번째 문자열을 읽어옴)
-        char operator= sc.next().charAt(0);
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            // 문자열을 읽고 charAt(0) 문자열에 몇번째것을 저장할건지 선택 (0번은 첫번째 문자열을 읽어옴)
+            char operator = sc.next().charAt(0);
 
-        System.out.println("operator = " + operator);
+            System.out.println("operator = " + operator);
 
-        int answer = 0;
+            int answer = 0;
 
-        if (operator == '+'){
-            answer = num + num2;
-        }else if (operator == '-'){
-            answer = num - num2;
-        }else if (operator == '*'){
-            answer = num * num2;
-        }else if (operator == '/'){
-            if (num2 != 0) {
-                answer = num / num2;
+            if (operator == '+') {
+                answer = num + num2;
+            } else if (operator == '-') {
+                answer = num - num2;
+            } else if (operator == '*') {
+                answer = num * num2;
+            } else if (operator == '/') {
+                if (num2 != 0) {
+                    answer = num / num2;
+                } else {
+                    System.out.println("두번째 입력한 숫자는 0이 될 수 없습니다.");
+                    return;
+                }
             } else {
-                System.out.println("두번째 입력한 숫자는 0이 될 수 없습니다.");
+                System.out.println("사칙여산 기호를 입력 해주세요");
                 return;
             }
-        }else {
-            System.out.println("사칙여산 기호를 입력 해주세요");
-            return;
-        }
 
-        System.out.println("결과: " + answer);
+            System.out.println("결과: " + answer);
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+        }while (!sc.next().equals("exit"));
+            System.out.println("계산이 종료되었습니다.");
     }
 }
